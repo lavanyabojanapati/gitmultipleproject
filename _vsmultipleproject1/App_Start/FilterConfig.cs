45db187f4d955471fd442635b3b3d596c3a41630
bnnavanyavs@outlook.com <bnnavanyavs@outlook.com>
2019-11-29 13:52:07 +0530

    Add project files.

---
+++ _vsmultipleproject1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _vsmultipleproject1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
